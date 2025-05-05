d274816b4deb5123f172b65c13a1057630a41c9d
Eric <ericjosephfrank@gmail.com>
2025-05-04 22:30:52 -0400

    Fix npm import

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// Core HSM implementation
+export { State } from "./core/State";
+export { HSM } from "./core/HSM";
+export { HSMConfigBuilder } from "./core/HSMConfigBuilder";
+export { HSMTypeBuilder } from "./core/HSMTypeBuilder";
+
+// Types
+export * from "./types/hsm";
+export * from "./types/events";
